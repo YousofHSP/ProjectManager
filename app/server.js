b4d31bbbbc8a7761889490e43e3f345208d48122
Yousof Hosseinpour <yousof.hosseinpour@gmail.com>
2022-06-05 23:35:50 +0430

    create server and config application

---
+++ app/server.js
@@ -0,0 +1,58 @@
+module.exports = class Application {
+    #express = require('express');
+    #app = this.#express();
+    constructor(port, DBURL) {
+        this.configApplication();
+        this.createServer(port);
+        this.configDatabase(DBURL);
+        this.createRoutes();
+        this.errorHandler();
+
+    }
+    configApplication(){
+        const path = require('path');
+        this.#app.use(this.#express.json());
+        this.#app.use(this.#express.urlencoded({extended: true}));
+        this.#app.use(this.#express.static(path.join(__dirname, "..", "public")));
+    }
+    createServer(port){
+        const http = require('http');
+        const server = http.createServer(this.#app);
+        server.listen(port, () =>{
+            console.log(`Server Run On http://localhost:${port}`);
+        });
+    }
+    configDatabase(DBURL){
+        const mongoose = require('mongoose');
+        mongoose.connect(DBURL, (error) => {
+            if(error) throw error;
+            return console.log("Connect DB Successful ...");
+        });
+    }
+    errorHandler(){
+        this.#app.use((req, res, next) => {
+            return res.status(404).json({
+                status: 404,
+                success: false,
+                message: "Page Not Found"
+            });
+        });
+        this.#app.use((error, req, res, next) => {
+            const status = error?.status || 500;
+            const message = error?.message || "Internal Server Error";
+
+            return res.status(status).json({
+                status,
+                message,
+                success: false
+            })
+        })
+    }
+    createRoutes(){
+        this.#app.get('/', (req, res, next) => {
+            return res.json({
+                message: "This is a new Express Application"
+            });
+        })
+    }
+}
